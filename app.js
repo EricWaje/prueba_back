808cf8684eddf950e3db2c7a84fab0cb162dc509
Eric Wajnrajch <ericwaje03@gmail.com>
2022-01-01 19:34:16 -0300

    modificando variables de entorno

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ app.post('/api/products', (req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`app levantada en el puerto ${PORT}`);
 });
